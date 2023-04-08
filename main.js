1bf804dd87fa694c7fef4c5c2e9c6655f1e16f1a
Karol Filas <filaskarol@gmail.com>
2023-04-08 10:05:30 +0200

    forEach version

--- main.js
+++ main.js
@@ -3,16 +3,22 @@ const passwords = ['jeDEN', 'dWa']
 const messages = ['super', 'działa!']
 const div = document.querySelector('div')
 
+// const showMessage = (e) => {
+//     div.textContent = ''
+//     for (let i = 0; i < passwords.length; i++) {
+//         const value = e.target.value.toLowerCase()
+//         if (passwords[i].toLocaleLowerCase() === value) {
+//             div.textContent = messages[i]
+//         }
+//     }
+// }
+
 const showMessage = (e) => {
-
-    div.textContent = ''
-    for (let i = 0; i < passwords.length; i++) {
-        const value = e.target.value.toLowerCase()
-        if (passwords[i].toLocaleLowerCase() === value) {
-
+    passwords.forEach((item, i) => {
+        if (item.toLocaleLowerCase() === e.target.value.toLocaleLowerCase()) {
             div.textContent = messages[i]
         }
-    }
+    })
 }
 
 input.addEventListener('input', showMessage)
