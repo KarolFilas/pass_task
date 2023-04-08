f1ac56fec608d3954e9edc59b1f8055352ae5a90
Karol Filas <filaskarol@gmail.com>
2023-04-08 10:40:06 +0200

    map version

--- main.js
+++ main.js
@@ -3,22 +3,39 @@ const passwords = ['jeDEN', 'dWa']
 const messages = ['super', 'działa!']
 const div = document.querySelector('div')
 
+const LCPasswors = passwords.map((password, index) => {
+    return password.toLocaleLowerCase()
+})
+
+const showMessage = (e) => {
+    const textInput = e.target.value.toLocaleLowerCase();
+    div.textContent = ''
+    for (let i = 0; i < LCPasswors.length; i++) {
+        if (textInput === LCPasswors[i]) {
+            div.textContent = messages[i]
+        }
+    }
+}
+
+console.log(LCPasswors);
+
 // const showMessage = (e) => {
 //     div.textContent = ''
+//     const value = e.target.value.toLowerCase()
 //     for (let i = 0; i < passwords.length; i++) {
-//         const value = e.target.value.toLowerCase()
+//
 //         if (passwords[i].toLocaleLowerCase() === value) {
 //             div.textContent = messages[i]
 //         }
 //     }
 // }
 
-const showMessage = (e) => {
-    passwords.forEach((item, i) => {
-        if (item.toLocaleLowerCase() === e.target.value.toLocaleLowerCase()) {
-            div.textContent = messages[i]
-        }
-    })
-}
+// const showMessage = (e) => {
+//     passwords.forEach((item, i) => {
+//         if (item.toLocaleLowerCase() === e.target.value.toLocaleLowerCase()) {
+//             div.textContent = messages[i]
+//         }
+//     })
+// }
 
 input.addEventListener('input', showMessage)
